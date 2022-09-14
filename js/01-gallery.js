b2927159dad71dfc336cce79bf7dccbf123e0a57
Iryna Bodnariuk <bodnariuk.iryna1@gmail.com>
2022-09-14 23:02:09 +0300

    02 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBoxRef = document.querySelector('.gallery');
+
+//створення картинок
+function creatingPictureMarkup(gallery) {
+  return gallery
+    .map(
+      ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join('');
+}
+
+galleryBoxRef.insertAdjacentHTML(
+  'beforeend',
+  creatingPictureMarkup(galleryItems)
+);
+
+//при кліку на зображення
+galleryBoxRef.addEventListener('click', openPicture);
+
+function openPicture(e) {
+  e.preventDefault(); // заборона на відкривання в новому вікні
+  // якщо не img
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+  //закриття Escape
+  document.addEventListener('keydown', clousePicture);
+  function clousePicture(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBoxRef = document.querySelector('.gallery');
+
+//створення картинок
+function creatingPictureMarkup(gallery) {
+  return gallery
+    .map(
+      ({
+        preview,
+        original,
+        description,
+      }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    )
+    .join('');
+}
+
+galleryBoxRef.insertAdjacentHTML(
+  'beforeend',
+  creatingPictureMarkup(galleryItems)
+);
+//при кліку на зображення
+galleryBoxRef.addEventListener('click', openPicture);
+
+function openPicture(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const gallery = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    captionsData: 'alt',
+  });
+}
